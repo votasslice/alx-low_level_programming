b80032968b3a6655ee22941910294ed492243c3d
votasslice <adeoluvic@gmail.com>
2022-09-11 11:39:39 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,8 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
-
-{
-    
-    puts("\"Programming is like building a multilingual puzzle");
-    
+{   
+    puts("\"Programming is like building a multilingual puzzle");   
     return (0);
-    
 }
