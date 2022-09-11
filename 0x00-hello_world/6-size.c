69f4c8d226adcc6bbf8d2e033104e0b21815b3ca
votasslice <adeoluvic@gmail.com>
2022-09-11 11:56:00 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-        
+
 return (0);
 }
