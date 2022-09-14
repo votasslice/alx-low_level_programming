4fed848419c81968d6c995611601e95059e54212
votasslice <adeoluvic@gmail.com>
2022-09-13 18:28:29 -0700

    C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - This is to print using printchar
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	_putchar("Print Nicely"\n)
+
+	return (0)
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Just read what you need
