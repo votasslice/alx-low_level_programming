a0c27b655e6d6e83667e948e4ac646827a770126
votasslice <adeoluvic@gmail.com>
2022-09-13 19:12:00 -0700

    C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
+
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{	
+for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-	}
-	putchar('\n');
+		putchar('\n');
 
-	return (0);
+return (0);
 }
