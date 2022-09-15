25acf2835c745ca9566212217415aa43ec3d7594
votasslice <adeoluvic@gmail.com>
2022-09-15 11:55:20 -0700

    C

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - Print all single digit numbers
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int digit;
