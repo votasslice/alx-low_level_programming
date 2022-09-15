4f9dd375746ae6f833e93ea01e92a5189834ec06
votasslice <adeoluvic@gmail.com>
2022-09-15 12:07:32 -0700

    C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - Print all single digit numbers using only putchar
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print all single digit numbers using only putchar
+ *
+ * Return: Always 0 
+ */
 int main(void)
 {
 	int digit;
 
-	for (digit = 0; digit < 10; digit++)
-	putchar(digit + '0');
-
-	putchar('\n');
+	for (digit =0; digit < 10; digit++)
+	putchar(digit);
+	putchar('\n')
 
 	return (0);
 }
