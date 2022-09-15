82d75712b6eb2455ceb0ee8156c960c1735cb8e3
votasslice <adeoluvic@gmail.com>
2022-09-15 12:12:50 -0700

    C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int digit;
 
-	for (digit =0; digit < 10; digit++)
+	for (digit =0; digit < 10; digit++);
 	putchar(digit + '0');
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
